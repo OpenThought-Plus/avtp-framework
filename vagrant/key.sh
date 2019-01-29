c97949100594318118c0979fa6cebd7bf55ed625
Chas Wareing <c3w@juicypop.net>
2019-01-29 08:12:49 -0800

    parm pass

--- vagrant/key.sh
+++ vagrant/key.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+if [ ! ${1} ]; then { echo "Usage: ./init.sh [dev|test|staging|production] [host]"; exit 1; }; fi
+
 ssh-add key ~/.ssh/id_rsa
-bash -c "./init.sh test"
+bash -c "./init.sh ${1}"
