2cc4815c6da6dc3c184840dbe099c3ec260af56c
Chas Wareing <c3w@juicypop.net>
2019-01-29 09:44:56 -0800

    pre install

--- vagrant/vagrant-ansible-installer.sh
+++ vagrant/vagrant-ansible-installer.sh
@@ -7,6 +7,6 @@
 sudo apt-get -y update
 sudo apt-get -y install python2.7
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-#sudo apt-get -y install libpq-dev postgresql-contrib
-#sudo apt-add-repository --yes --update ppa:ansible/ansible
-#sudo apt-get -y install ansible
+sudo apt-get -y install libpq-dev postgresql-contrib
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get -y install ansible
